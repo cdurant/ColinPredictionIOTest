c840d94dc9bbf77ffd46d7731c89a4dd87d48fba
Colin <colin@padopolis.com>
2014-06-09 16:25:01 -0700

    Fixing stupid client mistake

--- import.py
+++ import.py
@@ -7,14 +7,14 @@ random.seed()
 client = predictionio.Client(appkey="hkp4eZXspTjIJUhk7RJpEaO1YWO2V4t8QRHG4nZTjHzIJpNvX4K4r5AwanRnoAKv")
 ########################################################
 # generate 10 users with user ids 1,2,...,10
-user_ids = [str(i) for i in range(1,11)]
+user_ids = [str(i) for i in range(1, 11)]
 for user_id in user_ids:
 	print "Add user " + user_id
-	client.
+	client.create_user(user_id)
 ########################################################
 # generate 50 items with item ids 1,2,...,50
 # assign type id 1 to all of them
-item_ids = [str(i) for i in range(1,51)]
+item_ids = [str(i) for i in range(1, 51)]
 for item_id in item_ids:
 	print "Add item " + item_id
 	client.create_item(item_id, ('1',))
